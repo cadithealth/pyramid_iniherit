57c109827abcf00f29cbccf1d657fa335f979495
grabner <pjg.github@ubergrabner.net>
2013-08-26 14:55:49 -0400

    added watching of inherited files

--- pyramid_iniherit/install.py
+++ pyramid_iniherit/install.py
@@ -8,12 +8,29 @@
 
 '''
 Automatically installs the `iniherit` ConfigParsers as the default
-ConfigParsers on import.  WARNING: this is generally considered
+ConfigParsers on import. WARNING: this is generally considered
 "dangerous" -- use with extreme caution.
+
+Also installs an adujsted Loader that adds all inherited files to the
+:func:`pyramid.scripts.pserve.watch_file` list.
 '''
 
+# install overrides into ConfigParser
 import iniherit
+
 iniherit.mixin.install_globally()
+
+# install a "watching" file loader
+import iniherit.parser
+from pyramid.scripts.pserve import watch_file
+
+iniherit.parser._real_Loader = iniherit.parser.Loader
+class WatchingLoader(iniherit.parser._real_Loader):
+  def load(self, name, encoding=None):
+    try: watch_file(name)
+    except: pass
+    return iniherit.parser._real_Loader.load(self, name, encoding)
+iniherit.parser.Loader = WatchingLoader
 
 #------------------------------------------------------------------------------
 # end of $Id$
